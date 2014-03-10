fb8d935092b659aa41cafcdd1017c93bf20d11e2
AndreasDL <andreasdelille@hotmail;com>
2014-03-10 19:49:32 +0100

    dummy webservice

--- playground/service/database/AccessDatabase.php
+++ playground/service/database/AccessDatabase.php
@@ -8,6 +8,7 @@ class AccessDatabase{
     private $dbname;
     //database on localhost
     private $conString;
+    private $data;
     
     public function __construct(){
         $this->login  = 'postgres';
@@ -15,56 +16,47 @@ class AccessDatabase{
         $this->dbname = "testdb";
         
         $this->conString = "dbname=".$this->dbname." user=".$this->login." password=".$this->pass;
-        //echo $this->conString;
-    }
-    
-    public function getConnection(){
-        $con = pg_Connect($this->conString) or die('connection failed');
-        return $con;
+        
+        $this->data = array();
+        for ($i = 0 ; $i < 3 ; $i++){
+            $test = array();
+            $test['testname']  = 'ping';
+            $test['testbedId'] = "urn-testbed$i";
+            $test['planId']    = 103+$i;
+            $test['resultId']  = 1452 + $i;
+            $test['log']       = "http://www.".$test['testbedId']."com/Logs/".$test['testname']."/log".$test['resultId'];
+            $test['results']   = array();
+                $subResult = array();
+                $subResult['name'] = 'pingValue';
+                $subResult['value'] = rand(20,73);
+            array_push($test['results'],$subResult);
+            
+            array_push($this->data,$test);
+        }
+        
+        for ( $i = 0 ; $i < 5 ; $i++){
+            $test = array();
+            $test['testname']  = 'stitching';
+            $test['testbedId'] = "urn-testbed$i";
+            $test['planId']    = 78+$i;
+            $test['resultId']  = 2358 + $i;
+            $test['log']       = "http://www.".$test['testbedId']."com/Logs/".$test['testname']."/log".$test['resultId'];
+            $test['results']   = array();
+            
+            $subs = array('setup','getUserCredential','generateRspec','');
+                $subResult = array();
+                $subResult['name'] = 'setup';
+                $subResult['value'] = 'succes';
+            array_push($test['results'],$subResult);
+            
+            array_push($this->data,$test);
+        }
+        print_r($this->data);
     }
     
     public function getLast($params){
-        //print_r($params);
-        $ret = array();
-        $query = "select r1.* from results r1 "
-                    . "inner join ("
-                        . "select testbedid,max(resultid) as max from results "
-                        . "group by testbedid) r2 "
-                    . "ON r2.testbedid = r1.testbedid and r1.resultid = r2.max";
-        $paramsForUse = array();
+        $ret = $this->data;
         
-        #params
-        #tests => select values
-        if (array_key_exists('test',$query)){
-            
-        }
-        
-        #prepare query
-        #testbed => in where
-        #keep parameters to use in an array, then bind the array
-        #not given or set to all => no params added & all testbeds will be returned
-        if (array_key_exists('testbed',$params) && strtoupper($params['testbed'][0]) != 'ALL'){
-            #build $query
-            $query .= ' where r1.testbedid = $1';
-            array_push($paramsForUse , $params['testbed'][0]);
-            for ($i = 2; $i <= sizeof($params['testbed']) ; $i++){
-                $query .= " or r1.testbedid = \$$i";
-                array_push($paramsForUse , $params['testbed'][$i-1]);
-            }
-            //echo "$query<br><br>";
-            //print_r($paramsForUse);
-            #parameters binden
-        }
-        
-        //$data = pg_exec($this->getConnection(),$query);
-        $data = pg_query_params($this->getConnection(),$query,$paramsForUse);
-        
-        while ($row  = pg_fetch_assoc($data)){
-            //array me de rijtjes in
-            array_push($ret, $row);
-        }
-        
-        print_r($ret);
-        return $ret;
+        //return $ret;
     }
 }
