cf9aae5aaa716f08bfa315b7dbdf2b24a0bca2f9
andreasDL <andreasdelille@hotmail.com>
2014-06-07 17:28:01 +0200

    cleanup

--- playground/service/controllers/iController (stuffZ-Win7's conflicted copy 2014-06-05).php
+++
@@ -1,4 +0,0 @@
-<?php
- interface iController{
-     public function get($params);
- }
--- playground/service/database/dummydatabase/query (stuffZ-Win7's conflicted copy 2014-06-05).sql
+++
@@ -1,9 +0,0 @@
-with view as (
-	select *,r.resultid id from results r
-    	join (select * from subresults) sr on r.resultid = sr.resultid
-    	join (select * from testinstances) ti on ti.testinstanceid = r.testinstanceid
-    	join (select * from parameterinstances) pi on pi.testinstanceid = r.testinstanceid
-)
-select * from ( 
-	select *,dense_rank() over(partition by testname,testtype order by timestamp desc) rank from view
-) vv
