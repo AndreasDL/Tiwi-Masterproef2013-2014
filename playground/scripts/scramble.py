47e0ba95ab55ee749c9dfc322bba9f592afeceb0
andreasDL <andreasdelille@hotmail.com>
2014-06-20 18:07:08 +0200

    scramble script om de testen te verspreiden

---
+++ playground/scripts/scramble.py
@@ -0,0 +1,39 @@
+import psycopg2
+import sys
+import pprint
+import random
+
+
+dbname = "monitoring"
+user = "postgres"
+dass = "post"
+durl = "localhost"
+pingFreq   = 300
+listFreq   = 900
+getVerFreq = 900
+loginFreq  = 3600
+stitchFreq = 43200
+nextRun = "2014-1-7T12:00:00"
+enabled = True
+
+if (len(sys.argv) > 1):
+    dbname = str(sys.argv[1])
+
+print("Connecting to database")
+con = psycopg2.connect(database=dbname,user=user,password=dass,host=durl)
+cur = con.cursor()
+cus = con.cursor()
+
+getTests = "select testinstanceid,frequency from testinstances"
+changeNextRun = "update testinstances set nextrun = nextrun + interval '%s second' where testinstanceid = %s"
+
+cur.execute(getTests)
+test = cur.fetchone()
+while (test != None):
+	#pprint.pprint(test)
+	time = random.randint(0,test[1]-1);
+	cus.execute(changeNextRun,(time,test[0]))
+
+	test = cur.fetchone()
+
+con.commit()
